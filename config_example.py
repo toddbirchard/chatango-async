55cd0f21b645a5363d4a3d1cbcec64787ae9b673
Todd Birchard <toddbirchard@gmail.com>
2023-08-19 01:20:34 -0400

    Rename file structure.

--- config_example.py
+++
@@ -1,9 +0,0 @@
-"""Example config values for tutorial."""
-
-
-class Config:
-    """Example Chatango bot config values."""
-    prefix = ["!"]
-    owners = ["exampleuser"]
-    rooms = ["exampleroom"]
-    botuser = ["examplebot", ""]  # username, password
